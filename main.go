00bdd1135c5b171000ad7a39eea2d1e438aa8857
Nathaniel Wheeler <contact@nathanielwheeler.com>
2020-01-14 16:44:19 -0700

    time format

--- main.go
+++ main.go
@@ -4,33 +4,26 @@ import (
 	"log"
 	"os"
 	"text/template"
+	"time"
 )
 
 var tpl *template.Template
 
-type cat struct {
-	Name string
-	Nickname string
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("templates/index.gohtml"))
 }
 
-func init() {
-	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
+func yearMonthDay(t time.Time) string {
+	return t.Format("2006-01-02-Mon")
+}
+
+var fm = template.FuncMap{
+	"fdateYMD": yearMonthDay,
 }
 
 func main() {
-	sophie := cat{
-		Name: "Sophie",
-		Nickname: "Fluff",
-	}
 
-	cecy := cat{
-		Name: "Cecily",
-		Nickname: "Fuzz",
-	}
-
-	cats := []cat{sophie, cecy}
-
-	err := tpl.Execute(os.Stdout, cats)
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", time.Now())
 	if err != nil {
 		log.Fatalln(err)
 	}
